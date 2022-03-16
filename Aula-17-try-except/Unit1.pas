bdb5b204ae2e28c88d942b1f71989eeeab1e9fb7
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-16 17:24:20 -0400

    Aula 17 try except

---
+++ Aula-17-try-except/Unit1.pas
@@ -0,0 +1,40 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  i : Integer;
+  s : String;
+begin
+  try
+    i := StrToInt(Edit1.Text);
+    ShowMessage('Sucesso');
+  except
+    MessageDlg('Erro de conversão', mtError, [mbOk], 0);
+  end;
+end;
+
+end.
