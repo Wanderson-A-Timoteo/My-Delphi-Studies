1101b686b5c65074aa95d9a8e234a9365b61f1ab
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-18 12:39:13 -0400

    Adicionado layout da view principal e add sobrecarga na função calcular salario na classe vendedor

--- POO-20-Projeto/View.Principal.pas
+++ POO-20-Projeto/View.Principal.pas
@@ -4,10 +4,23 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Label1: TLabel;
+    cbFuncao: TComboBox;
+    Label2: TLabel;
+    edtNome: TEdit;
+    Label3: TLabel;
+    edtSalario: TEdit;
+    Label4: TLabel;
+    edtBonus: TEdit;
+    btnCadastrar: TButton;
+    Button1: TButton;
+    Button2: TButton;
+    Memo1: TMemo;
+    Button3: TButton;
   private
     { Private declarations }
   public
