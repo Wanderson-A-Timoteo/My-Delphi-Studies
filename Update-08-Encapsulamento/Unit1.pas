5842fe2132049493b7fbc82674fbfbceae72ce19
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-21 12:07:37 -0400

    Encapsulamento, exemplo classe cliente

---
+++ Update-08-Encapsulamento/Unit1.pas
@@ -0,0 +1,60 @@
+unit Unit1;
+{
+
+ ENCAPSULAMENTO tem como pricipio a criação de um objeto para encapsular
+ dentro dele todas as suas funções, metodos e atributos que ele precise para
+ funcionar sem depender de nenhum outro.
+
+ Isso possibilita seu uso em qualquer parte do código.
+
+}
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Memo.Types, FMX.ScrollBox,
+  FMX.Memo, Cliente;
+
+type
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+{ TForm1 }
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  Cliente : TCliente;
+
+begin
+  Cliente := Tcliente.Create;
+  try
+    Cliente.Nome := 'Wanderson';
+    Cliente.Telefone := '6532489444';
+    Cliente.Endereco := 'Rua da saudade';
+    Cliente.Cidade := 'Cuiabá';
+
+    Cliente.CadastrarCliente;
+  finally
+    Cliente.Free;
+  end;
+end;
+
+end.
