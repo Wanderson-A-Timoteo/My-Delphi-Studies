4a821151eddbcf17a1a4a170bad85b907637afd7
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-22 11:27:45 -0400

    Efetuado sobrecarga e chamado por herança

---
+++ Update-18-Heranca-Construtores/Classe.Pessoa.pas
@@ -0,0 +1,107 @@
+unit Classe.Pessoa;
+
+interface
+
+uses
+  System.Classes, System.SysUtils, Interfaces;
+
+type
+  TPessoa = class
+  private
+    FPrivate : String;
+
+  // Variaveis protected somente as classes filhas tem visibilidade
+  protected
+    FProtected : String;
+
+  public
+    Nome : String;
+    Telefone : String;
+    Endereco : String;
+    Cidade : String;
+    UF : String;
+    Conexao : IConexao;
+
+    // virtual -> Permite ser sobrecarregada
+    constructor Create(aConexao : IConexao); virtual;
+    procedure Cadastrar;
+    procedure CriarFinanceiro;
+    function GetFProtected : String;
+    function Tipo : String; virtual;
+
+  end;
+
+  // Outra classe
+  TMyComp = class(TComponent)
+    constructor Create(AOwner: TComponent); override;
+
+  end;
+
+implementation
+
+{ TCliente }
+
+procedure TPessoa.Cadastrar;
+var
+  Lista : TStringList;
+
+begin
+
+  Lista := TStringList.Create;
+
+  try
+    Lista.Add('Nome: ' + Nome);
+    Lista.Add('Telefone: ' + Telefone);
+    Lista.Add('Endereço: ' + Endereco);
+    Lista.Add('Cidade: ' + Cidade);
+    Lista.Add('UF: ' + UF);
+
+    Lista.SaveToFile(Nome + '_Cliente.txt');
+
+    Conexao.Gravar;
+
+  finally
+    Lista.Free;
+  end;
+
+end;
+
+constructor TPessoa.Create(aConexao : IConexao);
+begin
+  Conexao := aConexao;
+  UF := 'MT';
+end;
+
+procedure TPessoa.CriarFinanceiro;
+var
+  Lista : TStringList;
+begin
+  Lista := TStringList.Create;
+  try
+    Lista.Add('Nome: ' + Nome);
+    Lista.SaveToFile(Nome + '_Financeiro.txt');
+  finally
+    Lista.Free;
+  end;
+
+end;
+
+function TPessoa.GetFProtected: String;
+begin
+  Result := FProtected;
+end;
+
+function TPessoa.Tipo: String;
+begin
+  Result := 'Pessoa';
+end;
+
+{ TMyComp }
+// Efetuado a sobrecarga e a chamada por herança do componente
+constructor TMyComp.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+end;
+
+end.
