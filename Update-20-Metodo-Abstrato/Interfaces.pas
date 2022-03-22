fbfccb297f3f6da8aafe2575d67396a9b054c09b
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-22 12:31:51 -0400

    Metodo abstrato, exemplo classe Animal, em que as classes cachorro e gato herdam voz

---
+++ Update-20-Metodo-Abstrato/Interfaces.pas
@@ -0,0 +1,17 @@
+unit Interfaces;
+
+interface
+
+
+type
+  IConexao = interface
+  // Ctrl + Shift + G para criar a assinatura desta interface
+  ['{1ED0F7F1-E96A-477D-966D-67A72160EC3B}']     // Assinatura gerada
+
+  procedure Gravar;
+
+end;
+
+implementation
+
+end.
