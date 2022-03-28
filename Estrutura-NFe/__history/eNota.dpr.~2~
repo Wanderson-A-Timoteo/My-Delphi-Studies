b7bccb16d95af384a8cfe6eb93bf875f306ee10b
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-28 15:22:55 -0400

    Criando menus

---
+++ Estrutura-NFe/__history/eNota.dpr.~2~
@@ -0,0 +1,17 @@
+program eNota;
+
+uses
+  Vcl.Forms,
+  eNota.View.Principal in 'eNota.View.Principal.pas' {Form1},
+  eNota.Controller.NotaFiscal in 'eNota.Controller.NotaFiscal.pas',
+  eNota.Controller.NotaFiscal.Interfaces in 'eNota.Controller.NotaFiscal.Interfaces.pas',
+  eNota.Controller.NotaFiscal.Criar in 'eNota.Controller.NotaFiscal.Criar.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
--- Estrutura-NFe/eNota.dpr
+++ Estrutura-NFe/eNota.dpr
@@ -5,7 +5,12 @@ uses
   eNota.View.Principal in 'eNota.View.Principal.pas' {Form1},
   eNota.Controller.NotaFiscal in 'eNota.Controller.NotaFiscal.pas',
   eNota.Controller.NotaFiscal.Interfaces in 'eNota.Controller.NotaFiscal.Interfaces.pas',
-  eNota.Controller.NotaFiscal.Criar in 'eNota.Controller.NotaFiscal.Criar.pas';
+  eNota.Controller.NotaFiscal.Criar in 'eNota.Controller.NotaFiscal.Criar.pas',
+  eNota.Controller.NotaFiscal.Validar in 'eNota.Controller.NotaFiscal.Validar.pas',
+  eNota.Controller.NotaFiscal.Enviar in 'eNota.Controller.NotaFiscal.Enviar.pas',
+  eNota.Controller.NotaFiscal.Gravar in 'eNota.Controller.NotaFiscal.Gravar.pas',
+  eNota.Controller.NotaFiscal.Email in 'eNota.Controller.NotaFiscal.Email.pas',
+  eNota.Controller.Invoker in 'eNota.Controller.Invoker.pas';
 
 {$R *.res}
 
