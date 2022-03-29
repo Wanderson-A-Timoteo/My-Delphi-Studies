00d4850fd0ac98e74d9f87b63ac29f541a08eb03
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-29 17:12:56 -0400

    Criando entitade produtos

---
+++ Menus-em-MVC/Model/Entity/Menus.Model.Entity.Interfaces.pas
@@ -0,0 +1,17 @@
+unit Menus.Model.Entity.Interfaces;
+
+interface
+
+uses
+  System.Classes;
+
+type
+
+  iModelEntity = interface
+    ['{09DDC51A-AA19-4482-BEC5-5BBCBA74921A}']
+    function Listar : TComponent;
+  end;
+
+implementation
+
+end.
