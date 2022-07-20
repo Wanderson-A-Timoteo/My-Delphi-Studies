8d564c26cde519d720fcd57c98c59aecf5d465ef
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-07-20 15:42:38 -0400

    Adicionado exemplo Layout de um supermercado
    Layout de um supermercado com Design de Interface em FMX

---
+++ Layout_PS/Layout_PS/Delphi/Unit3.pas
@@ -0,0 +1,48 @@
+unit Unit3;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.ImageList, FMX.ImgList, FMX.StdCtrls,
+  FMX.Objects, FMX.Controls.Presentation, FMX.Layouts, FMX.MultiView, FMX.Edit, FMX.SearchBox, FMX.ListBox;
+
+type
+  TForm3 = class(TForm)
+    Layout1: TLayout;
+    Image1: TImage;
+    ToolBar1: TToolBar;
+    ToolBar2: TToolBar;
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+    Layout2: TLayout;
+    Image2: TImage;
+    SpeedButton1: TSpeedButton;
+    ImageList1: TImageList;
+    StyleBook1: TStyleBook;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    SpeedButton4: TSpeedButton;
+    Image3: TImage;
+    Image4: TImage;
+    Image5: TImage;
+    Image6: TImage;
+    Image7: TImage;
+    MultiView1: TMultiView;
+    Image8: TImage;
+    ListBox1: TListBox;
+    SearchBox1: TSearchBox;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.fmx}
+
+end.
